e1d0da08ea3a7b3303303af98c472a8de29f7bc9
Vivianatn <vivian.antoine@hotmail.com>
2025-12-10 14:14:43 +0100

    Sauvegarde Vivian 1

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AccueilController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'controller_name' => 'Accueil',
         ]);
     }
 }
